e78231e20938948216211f58459975d194f62b5d
meryvl <meryvalerolaguna@gmail.com>
2023-02-16 17:08:20 +0000

    RandomCard

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ window.onload = function() {
   const icons = ["♦", " ♥ ", "♠", "♣"];
   const numbers = ["Q", " k", "J", 1, 2, 3, 4, 5, 6, 7, 8, 9];
   document.querySelector(".card").innerHTML = `<div id='card' > 
-  <div id="palo">${icons[1]}</div>
+  <div id="suit">${icons[1]}</div>
   <div id="valor">${numbers[1]}</div>
-  <div id="palo2">${icons[1]}</div>
+  <div id="suit2">${icons[1]}</div>
 
 </div>`;
 
@@ -22,30 +22,30 @@ window.onload = function() {
       const icon = icons[numberRamdomIcon];
       const number = numbers[numberRandomValor];
       card.innerHTML = `
-      <div id="palo">${icon}</div>
+      <div id="suit">${icon}</div>
       <div id="valor">${number}</div>
-      <div id="palo2">${icon}</div>
+      <div id="suit2">${icon}</div>
       </div>`;
       if (icon === " ♥ ") {
         card.innerHTML = `<div class='card-heart red'> 
-        <div id="palo" >${icon}</div>
+        <div id="suit" >${icon}</div>
         <div id="valor" >${number}</div>
-        <div id="palo2" >${icon}</div></div>`;
+        <div id="suit2" >${icon}</div></div>`;
       } else if (icon === "♦") {
         card.innerHTML = `<div class='card-Diamond red'> 
-      <div id="palo" >${icon}</div>
+      <div id="suit" >${icon}</div>
       <div id="valor" >${number}</div>
-      <div id="palo2" >${icon}</div></div>`;
+      <div id="suit2" >${icon}</div></div>`;
       } else if (icon === "♠") {
         card.innerHTML = `<div class='card-spades'> 
-        <div id="palo" >${icon}</div>
+        <div id="suit" >${icon}</div>
         <div id="valor" >${number}</div>
-        <div id="palo2" >${icon}</div></div>`;
+        <div id="suit2" >${icon}</div></div>`;
       } else {
         card.innerHTML = `<div class='card-clubs'> 
-        <div id="palo" >${icon}</div>
+        <div id="suit" >${icon}</div>
         <div id="valor" >${number}</div>
-        <div id="palo2" >${icon}</div></div>`;
+        <div id="suit2" >${icon}</div></div>`;
       }
     };
     randomCard();
